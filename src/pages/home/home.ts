caf83ba56ec52be50480ff56e000744397944795
root <root@elpblr-025.evolvus.com>
2018-01-30 16:16:53 +0530

    initial  commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  options:CameraOptions;
+  takenPicture:any;
+  imgData:string;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private camera: Camera) {
+    this.options = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
   }
+
+  clickPicture(){
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.imgData=imageData;
+      alert(imageData[5]);
+      console.log('taken picture using camera:'+imageData);
+      this.takenPicture = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      console.log("failed to take picture:",err);
+      alert(err);
+     });
+    
+    
+  }
+
+  
 
 }
