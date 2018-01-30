c17efb321a92b11844ebc53901acd80b5d177bf4
root <root@elpblr-025.evolvus.com>
2018-01-30 18:49:05 +0530

    upated to  display image.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { LoadingController } from 'ionic-angular';
+
 
 
 
@@ -13,7 +15,7 @@ export class HomePage {
   takenPicture:any;
   imgData:string;
 
-  constructor(private camera: Camera) {
+  constructor(private camera: Camera,private loadingCtrl: LoadingController) {
     this.options = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
@@ -26,16 +28,28 @@ export class HomePage {
     this.camera.getPicture(this.options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
+      this.presentLoadingDefault();
       this.imgData=imageData;
-      alert(imageData[5]);
-      console.log('taken picture using camera:'+imageData);
-      this.takenPicture = 'data:image/jpeg;base64,' + imageData;
-     }, (err) => {
+
+      this.takenPicture = `data:image/jpeg;base64,${imageData}`;
+      
+   }, (err) => {
       console.log("failed to take picture:",err);
       alert(err);
      });
     
-    
+  }
+
+  presentLoadingDefault() {
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+  
+    loading.present();
+  
+    setTimeout(() => {
+      loading.dismiss();
+    }, 1000);
   }
 
   
